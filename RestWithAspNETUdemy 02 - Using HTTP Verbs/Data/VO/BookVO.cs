0550fa9c020a68a0776efba1fb7b260b9169956d
Johnatan Soares Modesto <jmodesto@bwg.com.br>
2019-07-29 14:13:42 -0300

    Implemented Data Contract

--- RestWithAspNETUdemy 02 - Using HTTP Verbs/Data/VO/BookVO.cs
+++ RestWithAspNETUdemy 02 - Using HTTP Verbs/Data/VO/BookVO.cs
@@ -1,9 +1,12 @@
 using System;
+using System.Runtime.Serialization;
 
 namespace RestWithAspNETUdemy.Data.VO
 {
+    [DataContract]
     public class BookVO
     {
+        [DataMember(Order = 1)]
         public long Id { get; set; }
         public string Title { get; set; }
         public string Author { get; set; }
--- RestWithAspNETUdemy 02 - Using HTTP Verbs/Data/VO/PersonVO.cs
+++ RestWithAspNETUdemy 02 - Using HTTP Verbs/Data/VO/PersonVO.cs
@@ -1,8 +1,12 @@
 
+using System.Runtime.Serialization;
+
 namespace RestWithAspNETUdemy.Data.VO
 {
+    [DataContract]
     public class PersonVO
     {
+        [DataMember(Order = 1)]
         public long Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
